01bb59bfcdf105a0af35d4b1ba9d4118e5d2bdfd
Luis D. Lafaurie <4016592+LudLaf@users.noreply.github.com>
2023-02-22 13:17:42 +0100

    Update game.py

---
+++ python-in-tmp/KylieYing/tutorial/game.py
@@ -0,0 +1,4 @@
+import random # basically this is existing code that you can call from your code
+
+for i in range(10): # basically, range(10) is 0, 1, 2,... 7, 8, 9,... do this thing 10 times 
+    print(random.randint(1, 10))
