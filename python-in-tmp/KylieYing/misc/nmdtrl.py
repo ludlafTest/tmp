1643e8e58d3c38deb0ba93014f7e38412f827b1a
Luis D. Lafaurie <4016592+LudLaf@users.noreply.github.com>
2023-02-22 13:09:07 +0100

    Add files via upload

---
+++ python-in-tmp/KylieYing/misc/nmdtrl.py
@@ -0,0 +1,15 @@
+fave_phrase = "Hello, uorld!"
+fave_number = 7
+
+# nm1 = str(input("frits name1: "))
+# nm2 = str(input("frits name2: "))
+# nm3 = str(input("frits name3: "))
+# nm4 = str(input("frits name4: "))
+# nm5 = str(input("frits name5: "))
+# nm6 = str(input("frits name6: "))
+# nm7 = str(input("frits name7: "))
+numberT = int(input("total numbrer: "))
+
+print(type(numberT))
+
+print(numberT)
